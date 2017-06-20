019f7ebd3de919a8c84c47513ab66d1450f25250
NicoAleC <chincholax@gmail.com>
2017-06-20 09:34:31 -0400

    clase producto

---
+++ ClienteVR/Producto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClienteVR
+{
+    class Producto
+    {
+        public int codproducto;
+        public int codC;
+        public double precioC;
+        public double precioV;
+        public int cantidad;
+        public EnvironmentVariableTarget imagen;
+        public string descripcionP;
+        public string nombreprod;
+    }
+}
